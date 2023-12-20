6770c9c973acdc279d6933ecc0602d9fcb5ad5f2
sumit bharodiya <sumitkumarbharodiya@gmail.com>
2023-12-20 12:32:39 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import Image from 'next/image'
+import RegisterForm from './components/RegisterForm'
+
+
+export default function Home() {
+  return (
+      <div >
+        <RegisterForm />
+      </div>
+  )
+}
